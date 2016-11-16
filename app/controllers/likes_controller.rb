4006bdd00557b844b7e4d570df8179a42d131d85
tail12 <tdallb12@gmail.com>
2016-11-16 23:26:25 +0900

    Like機能Ajaxで処理

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def like
-    idea = Idea.find(params[:idea_id])
-    like = current_user.likes.build(idea_id: idea.id)
+    @idea = Idea.find(params[:idea_id])
+    like = current_user.likes.build(idea_id: @idea.id)
     like.save
   end
 
   def unlike
-    idea = Idea.find(params[:idea_id])
-    like = current_user.likes.find_by(idea_id: idea.id)
+    @idea = Idea.find(params[:idea_id])
+    like = current_user.likes.find_by(idea_id: @idea.id)
     like.destroy
   end
 end
