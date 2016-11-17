21684619d5d4fcb84006d0b4cf77ede800dfdcb4
tail12 <tdallb12@gmail.com>
2016-11-17 19:33:51 +0900

    fixed flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      flash[:info] = "ログインしたぜ！！！＼(^o^)／"
+      flash[:info] = "ログインしました！"
       redirect_to @user
     else
-      flash[:danger] = 'ちゃんと入力しようぜ！確認大事'
+      flash[:danger] = '間違ってます！確認してください！'
       render 'new'
     end
   end
